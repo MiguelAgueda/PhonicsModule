199228dbd91515731c4f6939ce98c2dd4ff2d206
anthonysilva98 <asilva23@csustan.edu>
2020-04-24 21:44:30 -0700

    Math

--- phonicscards/src/views/About.vue
+++ phonicscards/src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Phonics Web Cards</h1>
+    <p>
+      Phonics web cards was created to promote learning.<br />
+      Please contact us at wqeqwe@gmail.com with complaints or problems.
+    </p>
   </div>
 </template>
