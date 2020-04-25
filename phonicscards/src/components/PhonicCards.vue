66e0b9f59488656fcf16c3ac5bc52be14323b67b
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-04-24 21:15:00 -0700

    Update

---
+++ phonicscards/src/components/PhonicCards.vue
@@ -0,0 +1,187 @@
+<template>
+  <b-row class="text-center">
+    <b-container>
+      <b-card-group deck class="pb-4">
+        <b-card img-src="Apple.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Balloon.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Caterpillar.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck>
+        <b-card img-src="Dog.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Egg.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Fish.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck class="pb-4">
+        <b-card img-src="Guitar.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Hat.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Igloo.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck>
+        <b-card img-src="Jar.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Key.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Lamp.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck class="pb-4">
+        <b-card img-src="Monkey.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Nest.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Octopus.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck>
+        <b-card img-src="Pencil.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Queen.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Ring.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck class="pb-4">
+        <b-card img-src="Snake.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Turtle.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Umbrella.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck>
+        <b-card img-src="Vacuum.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Wagon.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Box.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+
+      <b-card-group deck class="pb-4">
+        <b-card img-src="Yo-Yo.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+
+        <b-card img-src="Zipper.png" img-top style="max-width: 20rem;">
+          <b-card-text> </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-card-group>
+    </b-container>
+  </b-row>
+
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  name: 'PhonicCards',
+};
+</script>
